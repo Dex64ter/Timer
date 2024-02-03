f5471410009dd1def745378b3c5cc6b6647c3e9b
Davi J <davijosantos.dj@gmail.com>
2024-02-03 16:05:26 -0300

    Finalizando: Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -7,16 +7,23 @@ export function Home() {
       <form action="">
         <FormContainer>
           <label htmlFor="task" >Vou trabalhar em</label>
-          <TaskInput
-            id="task"
-            placeholder="Dê um nome para seu projeto"
-          />
+          <TaskInput id="task" placeholder="Dê um nome para seu projeto" list="task-suggestions"/>
+
+          <datalist id="task-suggestions">
+            <option value="Trabalhar no projeto" />
+            <option value="Estudar tecnologia" />
+            <option value="Trabalhar no TCC" />
+          </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
           <MinutesAmountInput
             id="minutesAmount"
             type="number"
             placeholder="00"
+            step={5}
+            max={60}
+            min={0}
+            
           />
           <span>minutos</span>
         </FormContainer>
